47ee5b6341a16246b658a51d404537d31e83369f
Angel Jadan <angel_jadan@outlook.com>
2018-03-27 21:07:11 -0500

    organizacion de archivos

--- app/src/main/java/xshadows/laboratorio_02/activities/MainActivity.java
+++ app/src/main/java/xshadows/laboratorio_02/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package xshadows.laboratorio_02;
+package xshadows.laboratorio_02.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import xshadows.laboratorio_02.R;
 
 public class MainActivity extends AppCompatActivity {
 
